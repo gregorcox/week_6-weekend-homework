e4b798647f8d3f407ae69c4652422264efb86135
Gregor Cox <gregorcox@gmail.com>
2018-06-29 16:41:10 +0100

    sets up guest class

--- src/main/java/Hotel/Guest.java
+++ src/main/java/Hotel/Guest.java
@@ -1,4 +1,14 @@
 package Hotel;
 
 public class Guest {
+
+    private String name;
+
+    public Guest(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
 }
---
+++ src/test/java/GuestTest.java
@@ -0,0 +1,20 @@
+import Hotel.Guest;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GuestTest {
+
+    Guest guest;
+
+    @Before
+    public void setup(){
+        guest = new Guest("John");
+    }
+
+    @Test
+    public void guestHasName(){
+        assertEquals("John", guest.getName());
+    }
+}
